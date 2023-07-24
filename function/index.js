5f2ae42ce2849bd1a784f1245596b0d94fb93855
sasan <137183548+Ch-alireza021@users.noreply.github.com>
2023-07-24 09:06:50 +0330

    function

---
+++ function/index.js
@@ -0,0 +1,97 @@
+// let a='salam sasan.khobi sasan?'
+// let b=a.replaceAll('sasan','mamad')
+// console.log(b)
+
+// let c=a.concat(' qazal ',b)
+// console.log(c)
+
+// let text = "55";
+// let padded = text.padStart(6,"xynvbknxdxbnjkxdgbn");
+// console.log(padded)
+
+
+// let number=[11,15,12,48,98,45,54,12]
+// console.log(number)
+
+// // add to end
+// console.log('push 85,75,65')
+// number.push(85,75,65);
+// console.log(number)
+
+// // rmove from end
+// console.log('pop') 
+// number.pop();
+// console.log(number)
+ 
+// // remove from start
+// console.log('shift')
+// number.shift();
+// console.log(number)
+
+// console.log("unshift '1','2'")
+// number.unshift('1','2');
+// console.log(number)
+
+// console.log(number.includes('2'))
+
+// let index=number.findIndex(()=>{
+//     item=48
+// })
+// console.log(number.indexOf(48))
+
+// console.log(index)
+
+
+// let forchi=number.forEach((item)=>{
+//     item+=10
+//     console.log(item)
+// })
+// console.log(forchi)
+
+// console.log('map\n\n\n\b')
+// let MAs=number.map((item)=>{
+//     item+=10
+//     console.log(item)
+// })
+// console.log(MAs)
+// --------------------------------------------------
+// let d=[1,2]
+// console.log(d);
+// console.log(d.concat([3,4]));
+// console.log(d.concat(5,6));
+
+
+
+// let users = [
+//     {id: 1, name: "John"},
+//     {id: 2, name: "Pete"},
+//     {id: 3, name: "Mary"}
+//   ];
+  
+//   let user = users.find(item => item.id == 1);
+  
+//   alert(user.name); // John
+
+// let users = [1,2,3,2,2];
+  
+//   let user = users.find(item => item== );
+  
+//   alert(user); // John
+
+
+//   let results = arr.filter(function(item, index, array) {
+//     // if true item is pushed to results and the iteration continues
+//     // returns empty array if nothing found
+//   });
+
+// console.log(users.indexOf(2))
+//  let a=users.filter((item,index)=>users.indexOf(item)===index);
+//  console.log(a);
+
+// const myArray = [1,2,3,2,2,9,8,7];
+// const mySet = new Set(myArray);
+// console.log(mySet);
+// console.log(mySet.add(6));
+// console.log(mySet.add('6'));
+// console.log(mySet.add({ channelName: 'JavaScript Mastery' }));
+// console.log(mySet.add([ 1, 2, 3 ]));
